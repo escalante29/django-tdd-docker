ac610688c657ddac9c9e60152c4285eae2bbec3f
cescalante-dev <carlos.dev.escalante@gmail.com>
2023-02-08 08:01:07 -0600

    partial commit - added code coverage and quality tools

--- app/movies/urls.py
+++ app/movies/urls.py
@@ -2,8 +2,7 @@
 
 from django.urls import path
 
-from .views import MovieList, MovieDetail
-
+from .views import MovieDetail, MovieList
 
 urlpatterns = [
     path("api/movies/", MovieList.as_view()),
--- app/tests/movies/test_views.py
+++ app/tests/movies/test_views.py
@@ -1,6 +1,6 @@
 # app/tests/movies/test_views.py
 
-import json
+# import json
 
 import pytest
 
@@ -19,7 +19,7 @@ def test_add_movie(client):
             "genre": "comedy",
             "year": "1998",
         },
-        content_type="application/json"
+        content_type="application/json",
     )
     assert resp.status_code == 201
     assert resp.data["title"] == "The Big Lebowski"
@@ -27,16 +27,13 @@ def test_add_movie(client):
     movies = Movie.objects.all()
     assert len(movies) == 1
 
+
 @pytest.mark.django_db
 def test_add_movie_invalid_json(client):
     movies = Movie.objects.all()
     assert len(movies) == 0
 
-    resp = client.post(
-        "/api/movies/",
-        {},
-        content_type="application/json"
-    )
+    resp = client.post("/api/movies/", {}, content_type="application/json")
     assert resp.status_code == 400
 
     movies = Movie.objects.all()
@@ -54,12 +51,13 @@ def test_add_movie_invalid_json_keys(client):
             "title": "The Big Lebowski",
             "genre": "comedy",
         },
-        content_type="application/json"
+        content_type="application/json",
     )
     assert resp.status_code == 400
 
     movies = Movie.objects.all()
     assert len(movies) == 0
+
 
 @pytest.mark.django_db
 def test_get_single_movie(client, add_movie):
@@ -70,14 +68,15 @@ def test_get_single_movie(client, add_movie):
 
 
 def test_get_single_movie_incorrect_id(client):
-    resp = client.get(f"/api/movies/foo/")
+    resp = client.get("/api/movies/foo/")
     assert resp.status_code == 404
+
 
 @pytest.mark.django_db
 def test_get_all_movies(client, add_movie):
     movie_one = add_movie(title="The Big Lebowski", genre="comedy", year="1998")
     movie_two = add_movie("No Country for Old Men", "thriller", "2007")
-    resp = client.get(f"/api/movies/")
+    resp = client.get("/api/movies/")
     assert resp.status_code == 200
     assert resp.data[0]["title"] == movie_one.title
     assert resp.data[1]["title"] == movie_two.title
